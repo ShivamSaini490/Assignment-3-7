88ebd4b5ce4f3905a147785a4ab423105a9c2f4c
shivam <shivam@DESKTOP-BT1O840>
2023-09-04 11:03:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MyClass
+{
+    private string privateProperty;
+
+    public MyClass(string initialValue)
+    {
+        privateProperty = initialValue;
+    }
+
+    public void SetPrivateProperty(string newValue)
+    {
+        privateProperty = newValue;
+    }
+
+    public string GetPrivateProperty()
+    {
+        return privateProperty;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass("Initial Value");
+
+        Console.WriteLine("Initial Private Property Value: " + myObject.GetPrivateProperty());
+
+        myObject.SetPrivateProperty("Modified Value");
+
+        Console.WriteLine("Modified Private Property Value: " + myObject.GetPrivateProperty());
+    }
+}
